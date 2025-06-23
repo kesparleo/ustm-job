f50a12488728957b27aa62f05ef88a453908233d
Enoque Mate <enoquesergio91@gmail.com>
2025-06-23 22:42:21 +0200

    atualizando para windows forms

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -1,6 +1,6 @@
 namespace T2.Utils
 {
     public class Constants {
-        public static string DATABASE_SOURCE = "Data Source=Database/bivdev.db";
+        public static string DATABASE_SOURCE = "Data Source=C:\\Users\\enoqu\\source\\Test2.fork\\Db\\bivlab.db";
     }
 }
